76dfaf30badb3a8484a88d9e05b8cd78ed8c9f4b
Thiago Rosa <thiagorcon@hotmail.com>
2023-12-11 10:36:28 -0300

    envio do template_exercicio

--- src/components/ListaTarefas/index.js
+++ src/components/ListaTarefas/index.js
@@ -11,16 +11,67 @@ import {
 import bin from "../../assets/bin.png";
 
 export function ListaTarefas() {
+  const listaDeTarefasIncial = [
+    { id: 1, titulo: "varrer casa" },
+    { id: 2, titulo: "limpar vidro" },
+    { id: 3, titulo: "molhar plantas" },
+  ];
   const [novaTarefa, setNovaTarefa] = useState("");
-
+  const [listaDeTarefas, setListaDeTarefas] = useState(listaDeTarefasIncial);
 
   const onChangeTarefa = (event) => {
     setNovaTarefa(event.target.value);
   };
 
-  const adicionaTarefa = () => {};
+  // ADICIONAR NO INICIO
+  const adicionaTarefa = () => {
+    if (novaTarefa === "") {
+    alert('favor digitar algo')
+    } else {
+      const novaTarefaComId = {
+        id: Date.now(),
+        titulo: novaTarefa,
+      };
+      const novaLista = [...listaDeTarefas];
+      novaLista.unshift(novaTarefaComId);
+      //Ou sem o unshift --- const novaLista = [novaTarefa , ...listaDeTarefas]
 
-  const removeTarefa = () => {};
+      setListaDeTarefas(novaLista);
+      //console.log(novaLista);
+      // -- para limpar o input
+      setNovaTarefa("");
+    }
+  };
+  // ADICIONAR NO FINAL
+  // const adicionaTarefa = () => {
+  //   const novaLista = [...listaDeTarefas]
+  //   novaLista.push(novaTarefa)
+  //  Ou sem o push    --- const novaLista = [...listaDeTarefas , novaTarefa]
+  //   setListaDeTarefas(novaLista)
+  //   //console.log(novaLista);
+  // };
+
+  const removeTarefa = (tarefaRemovida) => {
+    //alert(tarefaRemovida);
+    const listaFiltrada = listaDeTarefas.filter((tarefa) => {
+      if (tarefa.id !== tarefaRemovida.id) {
+        return tarefa;
+      }
+    });
+    setListaDeTarefas(listaFiltrada);
+  };
+
+  // Outra opção
+  // const listaDeTarefasMapeadas = listaDeTarefas.map((tarefa) => {
+  //   return (
+  //     <Tarefa>
+  //       <p>{tarefa}</p>
+  //       <RemoveButton>
+  //         <img src={bin} alt="" width="50px" />
+  //       </RemoveButton>
+  //     </Tarefa>
+  //   );
+  // });
 
   return (
     <ListaTarefasContainer>
@@ -30,17 +81,21 @@ export function ListaTarefas() {
           value={novaTarefa}
           onChange={onChangeTarefa}
         />
-        <AddTaskButton>Adicionar</AddTaskButton>
+        <AddTaskButton onClick={adicionaTarefa}>Adicionar</AddTaskButton>
       </InputContainer>
 
       <ListaContainer>
         <ul>
-          <Tarefa>
-            <p>Nova tarefa</p>
-            <RemoveButton>
-              <img src={bin} alt="" width="16px" />
-            </RemoveButton>
-          </Tarefa>
+          {listaDeTarefas.map((tarefa) => {
+            return (
+              <Tarefa key={tarefa.id}>
+                <p>{tarefa.titulo}</p>
+                <RemoveButton onClick={() => removeTarefa(tarefa)}>
+                  <img src={bin} alt="" width="50px" />
+                </RemoveButton>
+              </Tarefa>
+            );
+          })}
         </ul>
       </ListaContainer>
     </ListaTarefasContainer>
